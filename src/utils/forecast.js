b56574cef656ac3c0fccea4eb9753dbae276c658
bryan <30813784+bc3@users.noreply.github.com>
2021-01-19 19:48:02 -0800

    forecast and genlocation update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,48 @@
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+
+//query
+
+
+const forecast = (location, callback ) => {
+    //weatherstack request base url
+    const weatherstackBaseURL = 'http://api.weatherstack.com/current'
+
+    //access key
+    const wsKey = '28884348d5ca9149b951e1dd695a5278'
+
+    const weatherUrl = weatherstackBaseURL + '?access_key=' + wsKey + '&query=' + location
+
+    request({ url: weatherUrl, json: true }, (error, response) => {
+        if(error) {
+            // there was an error
+            //console.log('There was an error')
+            callback( 'Unable to connect to weather service', undefined)
+
+        }else if(!response.body.error){
+            //connection error
+            //console.log('Could not connect to server')
+            callback(error, undefined)
+        }else {
+            //everything is ok
+            console.log(response.body)
+
+            callback(undefined, response)
+
         }
-    })
+
+    } )
+
+
 }
 
+// forecast(-75.7088, 44.1545, (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
 module.exports = forecast
+
+//exports.forecast = forecast
+
