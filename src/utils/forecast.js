0cc9099d69cc9b4886e5b409abdd098b4358524b
bryan <30813784+bc3@users.noreply.github.com>
2021-01-20 01:15:22 -0800

    forecast class update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,9 +1,23 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
+    //const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
+    //weatherstack request base url
+    const weatherstackBaseURL = 'http://api.weatherstack.com/current'
 
-    request({ url: url, json: true }, (error, { body }) => {
+    //access key
+    const wsKey = '28884348d5ca9149b951e1dd695a5278'
+    
+    //location
+    const location = latitude + ', ' + longitude
+
+    //temperature units
+    const units = '&units=f'
+
+    //full combined weatherUrl
+    const weatherUrl = weatherstackBaseURL + '?access_key=' + wsKey + '&query=' + location + units
+
+    request({ url: weatherUrl, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
